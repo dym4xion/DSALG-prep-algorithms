90b17e8fed84d52ddf381de947807f4686c5edae
thesaladchalice <jayragoo@hotmail.co.uk>
2018-09-01 13:27:17 +0100

    finish quicksort.py

--- python/quicksort.py
+++ python/quicksort.py
@@ -1,1 +1,27 @@
 
+def quicksort(list):
+	less = []
+	equal = []
+	greater = []
+	
+	if len(list) > 1:
+		pivot = list[0]
+		for i in list:
+			if i < pivot:
+				less.append(i)
+			elif i == pivot:
+				equal.append(i)
+			else:
+				greater.append(i)
+		return quicksort(less) + equal + quicksort(greater)
+	else:
+		return list
+		
+# algorithm complete
+
+def main():
+	list = [5,10,1,3,16,7,20]
+	sortedList = quicksort(list)
+	print(sortedList)
+
+main()
