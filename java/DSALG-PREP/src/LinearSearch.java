01ebe9b9c35fb2cc86a662755c3d387b74199e2a
Aaryan Jay Ragoo <UP850844@uni.ds.port.ac.uk>
2018-09-25 12:14:37 +0100

    linearSearch.java changes

--- java/DSALG-PREP/src/LinearSearch.java
+++ java/DSALG-PREP/src/LinearSearch.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +14,26 @@
 public class LinearSearch {
     public static void main(String [] arg){
         System.out.println("LINEAR SEARCH!!!");
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(5);
+        list.add(8);
+        list.add(13);
+        list.add(21);
+        list.add(33);
+        System.out.println(list);
+        
+        linearSearch(list);
+    }
+    
+    public static void linearSearch(ArrayList list)
+    {
+        boolean inList = false;
+        Integer index = 0;
+        
+        
     }
 }
