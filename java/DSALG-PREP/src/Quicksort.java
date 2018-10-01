8cc710c2fbf9c72f11c74c603b835c3c0d6131c7
thesaladchalice <jayragoo@hotmail.co.uk>
2018-10-01 17:18:44 +0100

    started Quicksort.java

--- java/DSALG-PREP/src/Quicksort.java
+++ java/DSALG-PREP/src/Quicksort.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +14,49 @@
 public class Quicksort {
     public static void main(String [] arg){
         System.out.println("QUICKSORT!!!");
+        ArrayList<Integer> unsortedList = new ArrayList<Integer>();
+        unsortedList.add(5);
+        unsortedList.add(6);
+        unsortedList.add(7);
+        unsortedList.add(0);
+        unsortedList.add(9);
+        unsortedList.add(2018);
+        unsortedList.add(1998);
+        unsortedList.add(1);
+        System.out.println("Unsorted List: " + unsortedList);
+        
+        ArrayList sortedList = quicksort(unsortedList);
+        System.out.println("Sorted List: " + sortedList);
+    }
+    
+    public static ArrayList quicksort(ArrayList list)
+    {
+        ArrayList<Integer> less = new ArrayList<Integer>();
+        ArrayList<Integer> equal = new ArrayList<Integer>();
+        ArrayList<Integer> greater = new ArrayList<Integer>();
+        
+        if(list.size() > 1)
+        {
+            int pivot = (int) list.get(0);
+            for(int i = 0; i < list.size(); i++)
+            {
+               if((int)list.get(i) < pivot)
+               {
+                   less.add((int)list.get(i));
+               }else if(list.get(i).equals(pivot))
+               {
+                   equal.add((int)list.get(i));
+               }else
+               {
+                   greater.add((int)list.get(i));
+               }
+            }
+            return quicksort(less) + equal + quicksort(greater);
+                    
+        }else
+        {
+            return list;
+        }
+                
     }
 }
