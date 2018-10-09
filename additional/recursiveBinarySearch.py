d7061f24506d639c057fe256224244acb5d8c714
dym4xion <dym4xion@users.noreply.github.com>
2018-10-09 15:15:11 +0100

    progress on recursive binary search? why doesn't it work?

--- additional/recursiveBinarySearch.py
+++ additional/recursiveBinarySearch.py
@@ -1,16 +1,20 @@
 def recBinSearch(list, target, minima, maxima):
-	if minima == maxima:
-		return "target not in list"
-	elif list[int((minima + maxima) / 2)] == target:
-		return "target found"
-	elif target < int((minima + maxima) / 2):
-		return recBinSearch(list, target, minima, int((minima + maxima) / 2))
-	else:
-		return recBinSearch(list, target, int((minima + maxima) / 2), maxima)
+  mid = int((minima + maxima) / 2)
+  
+  if (minima == maxima):
+    return "target not in list"
+  elif list[mid] == target:
+    return "target found"
+  elif target < list[mid]:
+    return recBinSearch(list, target, minima, mid)
+  else:
+    return recBinSearch(list, target, mid, maxima)
 
 def main():
-	list = [1,2,3,4,5,6,7,8,9,10]
-	result = recBinSearch(list, 6, 0, len(list))
-	print(result)
+  list = [15,27,32,43,57,61,75,82,99,104]
+  result = recBinSearch(list, 82, 0, len(list))
+  print(result)
+
 main()
-##THIS IS WRONG!!!!
+## Still not working 
+## why???
