73780f8c6e4c4b9e174c6f4f11b1a8af2af76d68
dym4xion <dym4xion@users.noreply.github.com>
2018-10-09 12:33:39 +0100

    reursive version of binary search in python

---
+++ additional/recursiveBinarySearch.py
@@ -0,0 +1,15 @@
+def recBinSearch(list, target, minima, maxima):
+	if minima == maxima:
+		return "target not in list"
+	elif list[int((minima + maxima) / 2)] == target:
+		return "target found"
+	elif target < int((minima + maxima) / 2):
+		return recBinSearch(list, target, minima, int((minima + maxima) / 2))
+	else:
+		return recBinSearch(list, target, int((minima + maxima) / 2), maxima)
+
+def main():
+	list = [1,2,3,4,5,6,7,8,9,10]
+	result = recBinSearch(list, 6, 0, len(list))
+	print(result)
+main()
